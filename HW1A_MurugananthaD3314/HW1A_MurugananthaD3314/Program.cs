18562f2ea72afbcb69e6b015030c2b14ea9882e7
Divya Muruganantham <murugananthad3314@uhcl.edu>
2019-09-04 17:37:54 -0500

    Commit new changes

---
+++ HW1A_MurugananthaD3314/HW1A_MurugananthaD3314/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1A_MurugananthaD3314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number1 => ");
+            string input1 = Console.ReadLine();
+            int num1 = int.Parse(input1);
+
+            Console.WriteLine("Enter Number2 => ");
+            string input2 = Console.ReadLine();
+            int num2 = int.Parse(input2);
+
+            Console.WriteLine("Enter Operator (+, -, /, *) => ");
+            string input3 = Console.ReadLine();
+
+            int output = 0;
+
+            if (input3 == "+")
+            {
+                output = num1 + num2;
+            }
+            else if (input3 == "-")
+            {
+                output = num1 - num2;
+            }
+            else if (input3 == "*")
+            {
+                output = num1 * num2;
+            }
+            else if (input3 == "/")
+            {
+                output = num1 / num2;
+            }
+
+            Console.WriteLine(num1 + " " + input3 + " " + num2 + " = " + output + ".");
+            Console.ReadLine();
+        }
+    }
+}
